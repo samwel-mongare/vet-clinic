2bc1ec8c498a6ca68a0eb63a40659de6cb816cab
mongare <mongaresams@gmail.com>
2022-02-18 12:07:58 +0300

    Update species data on table

--- queries.sql
+++ queries.sql
@@ -30,3 +30,41 @@ SELECT * from animals WHERE name != 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg
 
 SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- TRANSACTIONS
+-- FIRST TRANSACTION
+
+BEGIN TRANSACTION;
+
+-- update the animals table by setting the species column to unspecified
+
+UPDATE animals
+ SET species = 'unspecified';
+
+--  roll back the change and verify that species columns went back to the state before transaction
+
+ROLLBACK TRANSACTION;
+
+-- SECOND TRANSACTION
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+
+UPDATE animals 
+SET 
+    species = 'digimon'
+WHERE
+    name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
+UPDATE animals 
+SET 
+    species = 'pokemon'
+WHERE
+    species IS NULL;
+
+    -- Inside a transaction delete all records in the animals table, then roll back the transaction.
+
+    TRUNCATE TABLE animals;
+
+    ROLLBACK TRANSACTION;
