88a9cfcd99e96e71d296de65e0f0cb9a9e38fdf6
mongare <mongaresams@gmail.com>
2022-02-18 17:41:52 +0300

    Modify animals table

--- schema.sql
+++ schema.sql
@@ -25,3 +25,36 @@ ALTER TABLE animals
     name varchar(100),
     PRIMARY KEY(id)
 );
+
+-- Modify animals table:
+
+    -- Making id set as autoincremented PRIMARY KEY
+   
+    ALTER TABLE animals 
+    DROP COLUMN id;
+
+    ALTER TABLE animals 
+    ADD COLUMN id integer GENERATED ALWAYS AS IDENTITY;
+
+    ALTER TABLE animals 
+    ADD PRIMARY KEY (id);
+
+    -- Remove column species
+
+    ALTER TABLE animals 
+    DROP COLUMN species;
+
+    -- Add column species_id which is a foreign key referencing species table
+
+     ALTER TABLE animals 
+    ADD COLUMN species_id integer;
+
+    ALTER TABLE animals 
+    ADD FOREIGN KEY (species_id) REFERENCES species;
+
+    -- Add column owner_id which is a foreign key referencing the owners table
+    ALTER TABLE animals 
+    ADD COLUMN owner_id integer;
+
+    ALTER TABLE animals 
+    ADD FOREIGN KEY (owner_id) REFERENCES owners;
